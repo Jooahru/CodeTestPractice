c9d68d1ad7668f1030b5fadb675a4657a75a7d1f
WILL <dndlf333@naver.com>
2022-09-14 23:48:19 +0900

    짝수와 홀수 단순풀이 + 삼항연산자

---
+++ CodeTestPractice/src/main/java/EvenOrOdd.java
@@ -0,0 +1,23 @@
+public class EvenOrOdd {
+    String evenOrOdd(int num) {
+        String answer = "";
+        if(num%2 == 0){
+            answer = "Even";
+        }else{
+            answer = "Odd";
+        }
+        return answer;
+    }
+
+    String evenOrOdd2(int num) {
+        return num % 2 == 0 ? "Even": "Odd";
+    }
+
+    public static void main(String[] args) {
+        EvenOrOdd evenOrOdd = new EvenOrOdd();
+        System.out.println("결과 : " + evenOrOdd.evenOrOdd(3));
+        System.out.println("결과 : " + evenOrOdd.evenOrOdd(2));
+        System.out.println("결과 : " + evenOrOdd.evenOrOdd2(3));
+        System.out.println("결과 : " + evenOrOdd.evenOrOdd2(2));
+    }
+}
