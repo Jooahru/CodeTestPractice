23504fb95fe34a7ffafe9fbfda55bf1fc422322f
WILL <dndlf333@naver.com>
2022-09-15 00:02:41 +0900

    자릿수 덧셈 while문 방식 + int/string 변환 방식

---
+++ CodeTestPractice/src/main/java/DigitAdd.java
@@ -0,0 +1,32 @@
+public class DigitAdd {
+
+    int digitAdd(int n) {
+        int answer = 0;
+
+        String[] NumberToString = Integer.toString(n).split("");
+        for (String Number : NumberToString) {
+            answer += Integer.parseInt(Number);
+        }
+
+        return answer;
+    }
+
+    public int digitAdd2(int n) {
+        int answer = 0;
+
+        while (n != 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        DigitAdd digitAdd = new DigitAdd();
+        System.out.println("결과 : " + digitAdd.digitAdd(123));
+        System.out.println("결과 : " + digitAdd.digitAdd(456));
+        System.out.println("결과 : " + digitAdd.digitAdd2(333));
+        System.out.println("결과 : " + digitAdd.digitAdd2(123));
+    }
+}
