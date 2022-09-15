b0e3360026be9ef1323e1c1a7284bb6fd81f9cb3
WILL <dndlf333@naver.com>
2022-09-15 23:53:34 +0900

    배열의 평균값 구하기 1. 기본코드 2. 라이브러리 사용 3. 널체크

---
+++ CodeTestPractice/src/main/java/GetAverage.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class GetAverage {
+
+
+    public double solution(int[] arr) {
+        double answer = 0;
+        for (int j : arr) {
+            answer += j;
+        }
+        answer /= arr.length;
+        return answer;
+    }
+
+    public int solution2(int[] array) {
+        return (int) Arrays.stream(array).average().orElse(0);
+    }
+
+    public int solution3(int[] array) {
+        if (array == null) {
+            return 0;
+        }
+        int ret = 0;
+            for (int n : array) {
+                ret += n;
+            }
+        return ret / array.length;
+    }
+
+    public static void main(String[] args) {
+        int x[] = {5, 4, 3};
+        GetAverage getAverage = new GetAverage();
+        System.out.println(" 평균값 : " + getAverage.solution(x));
+        System.out.println(" 평균값 : " + getAverage.solution2(x));
+        System.out.println(" 평균값 : " + getAverage.solution3(x));
+    }
+}
