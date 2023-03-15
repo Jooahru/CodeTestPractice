1d81818f64a64896c61365198e1bd14d73f68a17
WILL <dndlf333@naver.com>
2023-03-15 16:16:25 +0900

    [추가] 문자열에서 일치하는 문자 수 찾아내기

---
+++ CodeTestPractice/src/main/java/CountCharInString.java
@@ -0,0 +1,33 @@
+
+
+
+import java.util.Scanner;
+
+public class CountCharInString {
+	/*
+	 문자열에서 일치하는 문자 찾기
+	 */
+	public int solution(String str, char t) {
+		int answer = 0;
+		str = str.toUpperCase();
+		t= Character.toUpperCase(t);
+		
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)==t) {
+				answer++;
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		CountCharInString T = new CountCharInString();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		char c = kb.next().charAt(0);
+		kb.close();
+		System.out.println(T.solution(str, c));
+
+	}
+
+}
