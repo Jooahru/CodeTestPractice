5eaf72ad15b8e0061094a53b048ad116756d5dad
WILL <dndlf333@naver.com>
2023-03-16 23:47:26 +0900

    [추가] 문자열 대소문자 바꾸는 코드

---
+++ CodeTestPractice/src/main/java/ConvertLetters.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class ConvertLetters {
+
+    public String solution(String str) {
+        String answer = "";
+        for (char x : str.toCharArray()) {
+            if (Character.isLowerCase(x)) {
+                answer += Character.toUpperCase(x);
+            } else {
+                answer += Character.toLowerCase(x);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        ConvertLetters solution = new ConvertLetters();
+        System.out.println("solution = " + solution.solution(str));
+    }
+}
+
