7445133a00c5917b43be6a142ef6b46d112eb0d1
WILL <dndlf333@naver.com>
2023-03-17 00:21:08 +0900

    [추가] 문자열에서 가장 긴 단어 찾기

---
+++ CodeTestPractice/src/main/java/FindLongWord.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FindLongWord {
+
+    public String Solution(String str) {
+        String answer = "";
+        String[] s = str.split(" ");
+        for (String x : s) {
+            if (answer.length() < x.length()) {
+                answer = x;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FindLongWord findLongWord = new FindLongWord();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+        System.out.println("answer = " + findLongWord.Solution(str));
+
+    }
+
+}
