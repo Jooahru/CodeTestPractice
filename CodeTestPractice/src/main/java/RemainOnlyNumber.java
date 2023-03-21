03081531875e1ec1286fa6a4ca0a5df7d6e29c6b
WILL <dndlf333@naver.com>
2023-03-21 23:38:08 +0900

    [추가] 숫자만 남기기

---
+++ CodeTestPractice/src/main/java/RemainOnlyNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/*
+
+*/
+public class RemainOnlyNumber {
+
+    public int solution(String str){
+       int answer = 0;
+       for(char x : str.toCharArray()){
+           if(x>=48 && x<=57){
+               answer=answer*10+(x-48);
+           }
+       }
+       return answer;
+    }
+
+    public static void main(String[] args) {
+        RemainOnlyNumber remainOnlyNumber = new RemainOnlyNumber();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(remainOnlyNumber.solution(str));
+    }
+
+}
