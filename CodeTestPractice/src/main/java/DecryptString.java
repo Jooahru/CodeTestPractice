9be3ac9865f93b0bd1ce6a731e335d353e84beea
WILL <dndlf333@naver.com>
2023-03-23 23:02:50 +0900

    [추가] 암호화 문자열 해독

---
+++ CodeTestPractice/src/main/java/DecryptString.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DecryptString {
+
+    public String solution(String str, int num) {
+        String answer = "";
+        String tmp = "";
+        for (int i = 0; i < num; i++) {
+            tmp = str.substring(0, 7).replace("#", "1").replace("*", "0");
+            answer += (char) Integer.parseInt(tmp, 2);
+            str = str.substring(7);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        DecryptString main = new DecryptString();
+        Scanner kb = new Scanner(System.in);
+        int num = kb.nextInt();
+        String str = kb.next();
+        System.out.println(main.solution(str, num));
+
+    }
+
+}
