2a8458fe77d63ec7f88f6b9476d7a3018745c428
WOOIL KIM <dndlf_93@torpedo.co.kr>
2023-03-24 11:23:14 +0900

    [추가] 자신보다 큰 수 출력하기

---
+++ CodeTestPractice/src/main/java/PrintBigNumber.java
@@ -0,0 +1,38 @@
+package test;
+
+import java.util.Scanner;
+
+public class PrintBigNumber {
+
+	public String solution(String str, int num) {
+		String answer = "";
+		String[] numbs = str.split(" ");
+		for (String x : numbs) {
+
+			System.out.println(x);
+		}
+		System.out.println(numbs.length);
+		for (int i = 0; i < num; i++) {
+			if (i == 0) {
+				answer += numbs[i];
+			} else {
+				if (Integer.parseInt(numbs[i]) > Integer.parseInt(numbs[i-1])) {
+					answer += numbs[i];
+				}
+			}
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		PrintBigNumber main = new PrintBigNumber();
+		Scanner kb = new Scanner(System.in);
+		int num = kb.nextInt();
+		kb.nextLine();
+		String str = kb.nextLine();
+		System.out.println(main.solution(str, num));
+
+	}
+
+}
