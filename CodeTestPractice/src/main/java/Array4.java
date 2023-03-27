ea2d57f793f9b25052f9a0084275b6fc7c06332d
WILL <dndlf333@naver.com>
2023-03-27 15:19:09 +0900

    피보나치 수열 구하기

---
+++ CodeTestPractice/src/main/java/Array4.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+/*피보나치 수열 구하기*/
+public class Array4 {
+
+	public int[] solution(int num) {
+		int[] answer = new int[num];
+		answer[0] = 1;
+		answer[1] = 1;
+		for (int i = 2; i < num; i++) {
+			answer[i] = answer[i - 1] + answer[i - 2];
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Array4 array4 = new Array4();
+		Scanner kb = new Scanner(System.in);
+		int num = kb.nextInt();
+		int[] numArray = new int[num];
+		for (int x : array4.solution(num)) {
+			System.out.print(x + " ");
+		}
+
+	}
+
+}
