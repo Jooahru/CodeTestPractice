e3afc356b7e0423c9cd0201ab75aa1376ed80ad7
WILL <dndlf333@naver.com>
2023-04-03 23:35:11 +0900

    [추가] 두 배열 합치기

---
+++ CodeTestPractice/src/main/java/towPointerAndSlidingWindow/SumTwoArray.java
@@ -0,0 +1,51 @@
+package towPointerAndSlidingWindow;
+
+import java.util.Scanner;
+
+/*두 배열 합치기*/
+public class SumTwoArray {
+
+    public int[] solution(int[] numArray1, int[] numArray2, int num) {
+        int[] answer = new int[num];
+        int p1 = 0, p2 = 0, p3 = 0;
+        while (p1 < numArray1.length && p2 < numArray2.length) {
+            if (numArray1[p1] < numArray2[p2]) {
+                answer[p3] = (numArray1[p1++]);
+                p3++;
+            } else {
+                answer[p3] = (numArray2[p2++]);
+                p3++;
+            }
+        }
+        while (p1 < numArray1.length) {
+            answer[p3] = (numArray1[p1++]);
+            p3++;
+        }
+        while (p2 < numArray2.length) {
+            answer[p3] = (numArray2[p2++]);
+            p3++;
+        }
+
+        return answer;
+
+    }
+
+    public static void main(String[] args) {
+        SumTwoArray sumTwoArray = new SumTwoArray();
+        Scanner kb = new Scanner(System.in);
+        int num1 = kb.nextInt();
+        int[] numArray1 = new int[num1];
+        for (int i = 0; i < num1; i++) {
+            numArray1[i] = kb.nextInt();
+        }
+        int num2 = kb.nextInt();
+        int[] numArray2 = new int[num2];
+        for (int i = 0; i < num2; i++) {
+            numArray2[i] = kb.nextInt();
+        }
+        for(int x: sumTwoArray.solution(numArray1, numArray2, num1 + num2)){
+            System.out.print(x+" ");
+        }
+    }
+
+}
