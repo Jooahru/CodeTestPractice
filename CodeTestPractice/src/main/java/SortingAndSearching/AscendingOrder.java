18772313d7a1ef025722a24f4046b40a07f0d869
WILL <dndlf333@naver.com>
2023-04-26 16:12:50 +0900

    [수정] 선택정렬 정답출력로직 변경

--- CodeTestPractice/src/main/java/SortingAndSearching/AscendingOrder.java
+++ CodeTestPractice/src/main/java/SortingAndSearching/AscendingOrder.java
@@ -3,33 +3,34 @@ package SortingAndSearching;
 import java.util.Scanner;
 
 public class AscendingOrder {
-    public int[] solution(int number, int[] numbers){
-        int[] answer = new int[number];
+	public int[] solution(int number, int[] numbers) {
 
-        for(int i = 0 ;i<number-1;i++){
-            int idx = i;
-            for(int j = i+1;j<number;j++){
-                if(numbers[j]<numbers[idx]){
-                    idx = j;
-                }
-            }
-            int tmp=numbers[i];
-            numbers[i]=numbers[idx];
-            numbers[idx]=tmp;
+		for (int i = 0; i < number - 1; i++) {
+			int idx = i;
+			for (int j = i + 1; j < number; j++) {
+				if (numbers[j] < numbers[idx]) {
+					idx = j;
+				}
+			}
+			int tmp = numbers[i];
+			numbers[i] = numbers[idx];
+			numbers[idx] = tmp;
 
-        }
-        return answer;
-    }
+		}
+		return numbers;
+	}
 
-    public static void main(String[] args){
-        AscendingOrder ascendingOrder = new AscendingOrder();
-        Scanner kb = new Scanner(System.in);
-        int number = kb.nextInt();
-        int[] numbers = new int[number];
-        for(int i =0;i<number;i++){
-            numbers[i] = kb.nextInt();
-        }
-        System.out.println(ascendingOrder.solution(number,numbers));
-    }
+	public static void main(String[] args) {
+		AscendingOrder ascendingOrder = new AscendingOrder();
+		Scanner kb = new Scanner(System.in);
+		int number = kb.nextInt();
+		int[] numbers = new int[number];
+		for (int i = 0; i < number; i++) {
+			numbers[i] = kb.nextInt();
+		}
+		for (int x : ascendingOrder.solution(number, numbers)) {
+			System.out.print(x + " ");
+		}
+	}
 
 }
