318399f5bbbc1ec8f993e49338fb575029f2701e
WILL <dndlf333@naver.com>
2023-05-09 23:04:50 +0900

    [추가] 재귀함수를 사용한 피보나치 수열 나열하기 (성능개선)

--- CodeTestPractice/src/main/java/recursiveAndTreeAndGraph/FibonacciNumberSum.java
+++ CodeTestPractice/src/main/java/recursiveAndTreeAndGraph/FibonacciNumberSum.java
@@ -4,22 +4,29 @@ import java.util.Scanner;
 
 public class FibonacciNumberSum {
 
-    public int solution(int number) {
-        if (number == 1) {
-            return 1;
-        } else if (number == 2) {
-            return 1;
-        } else {
-            return solution(number - 2) + solution(number - 1);
-        }
+    static int[] fibo;
 
+    public int solution(int number) {
+        if (fibo[number] > 0) {
+            return fibo[number];
+        }
+        if (number == 1) {
+            return fibo[number] = 1;
+        } else if (number == 2) {
+            return fibo[number] = 1;
+        } else {
+            return fibo[number] = solution(number - 2) + solution(number - 1);
+        }
     }
 
     public static void main(String[] args) {
         FibonacciNumberSum fibonacciNumberSum = new FibonacciNumberSum();
         Scanner kb = new Scanner(System.in);
         int number = kb.nextInt();
-        System.out.println(fibonacciNumberSum.solution(number));
+        fibo = new int[number + 1];
+        fibonacciNumberSum.solution(number);
+        for (int i = 1; i <= number; i++) {
+            System.out.print(fibo[i] + " ");
+        }
     }
-
 }
