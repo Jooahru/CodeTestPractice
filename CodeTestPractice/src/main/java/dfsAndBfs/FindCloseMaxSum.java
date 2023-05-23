e631e1023675ba7cd7a9bcbf0af4f0efe13d7b3d
WILL <dndlf333@naver.com>
2023-05-23 23:18:41 +0900

    [추가] 기준값에 가장 가까운 합 구하기

---
+++ CodeTestPractice/src/main/java/dfsAndBfs/FindCloseMaxSum.java
@@ -0,0 +1,37 @@
+package dfsAndBfs;
+
+import java.util.Scanner;
+
+public class FindCloseMaxSum {
+
+    static int answer = 0;
+    static int number, cutWeight = 0;
+
+    public void DFS(int level, int sum, int[] weights) {
+        if (level == number) {
+            if (sum <= cutWeight && sum > answer) {
+                answer = sum;
+            }
+            return;
+        } else {
+            DFS(level + 1, sum + weights[level], weights);
+            DFS(level + 1, sum, weights);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        FindCloseMaxSum findCloseMaxSum = new FindCloseMaxSum();
+        Scanner kb = new Scanner(System.in);
+        cutWeight = kb.nextInt();
+        number = kb.nextInt();
+        int[] weights = new int[number];
+        for (int i = 0; i < number; i++) {
+            weights[i] = kb.nextInt();
+        }
+        findCloseMaxSum.DFS(0, 0, weights);
+        System.out.println(answer);
+
+    }
+
+}
